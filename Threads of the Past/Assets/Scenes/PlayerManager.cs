0af3b240e595702279f2a12343ff1de22cff4dc8
eviKS23 <eks2733@rit.edu>
2024-10-04 18:49:44 -0400

    This doesn't work

--- Threads of the Past/Assets/Scenes/PlayerManager.cs
+++ Threads of the Past/Assets/Scenes/PlayerManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
     //FIELDS
     [SerializeField]
-    private GameObject playerObject;
+    private SpriteRenderer playerObject;
 
     [SerializeField]
     private Collider2D childCollider;
@@ -33,6 +34,26 @@ public class PlayerManager : MonoBehaviour
     {
         if (inPast == true)
         {
+            playerObject.sprite = childSprite.sprite;
+        }
+        else
+        {
+            playerObject.sprite = adultSprite.sprite;
+        }
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if ((context.phase == InputActionPhase.Performed))
+        {
+            if (inPast == false)
+            {
+                inPast = true;
+            }
+            else if (inPast == true)
+            {
+                inPast = false;
+            }
             
         }
     }
