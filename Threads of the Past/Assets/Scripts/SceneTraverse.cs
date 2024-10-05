6cc5cd28fe14745550273f64cfbe50b54286d25c
secrectstick <2004ejp@gmail.com>
2024-10-05 15:36:34 -0400

    testing

---
+++ Threads of the Past/Assets/Scripts/SceneTraverse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTraverse : MonoBehaviour
+{
+
+    public int CurrentSceneIndex;
+    public int NextSceneIndex;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(CurrentSceneIndex);
+        }
+
+
+
+
+    }
+}
